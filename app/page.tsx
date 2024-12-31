c0fb41c18fc91a745412cc9c4135858001f667b9
Blessed Bagor <blessedbagor.eg@gmail.com>
2024-12-31 23:28:08 +0800

    Creating Dashboard Layout and Pages

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
         <IgiftLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -27,7 +26,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
