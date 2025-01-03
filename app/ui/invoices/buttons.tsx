d1df1ff611eb2509ad1f1f62e241d7199171bda5
Blessed Bagor <blessedbagor.eg@gmail.com>
2025-01-03 19:37:30 +0800

    Added form validation for Create, Update & Delete Invoice. Fixed <form action={...}> error

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,15 @@
+'use client';
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice, } from '@/app/lib/actions';
+import { useActionState } from 'react';
+
+export type State = {
+  message: string | null;
+  success: boolean;
+  error?: string;
+};
 
 export function CreateInvoice() {
   return (
@@ -24,13 +34,17 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+
+export function DeleteInvoice({ id }: { id: string }) { 
+  const initialState: State = { message: null, success: false, error: undefined };
+  const [state, formAction] = useActionState(deleteInvoice.bind(null, id), initialState);
+  
   return (
-    <>
+    <form action={formAction}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
